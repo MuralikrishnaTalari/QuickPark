e1945855ebc5d2f67c3b368e6963e69201c4a643
MuralikrishnaTalari <S3027386@live.tees.ac.uk>
2024-11-26 12:32:37 +0000

    Commit 7

--- app/src/main/java/uk/ac/tees/mad/univid/screens/HomeScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/univid/screens/HomeScreen.kt
@@ -1,12 +1,28 @@
 package uk.ac.tees.mad.univid.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
 import uk.ac.tees.mad.univid.viewmodel.MainViewModel
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
 
 @Composable
 fun HomeScreen(navController: NavHostController, viewModel: MainViewModel) {
-    Text(text = "Home Screen", fontSize = 40.sp)
+    val cameraPositionState = rememberCameraPositionState{
+        position = CameraPosition.fromLatLngZoom(LatLng(51.5074,-0.1278), 10f)
+    }
+    GoogleMap(modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState){
+        Marker(state = MarkerState(position = LatLng(51.5074,-0.1278)), title = "", snippet = "Availble")
+    }
 }
